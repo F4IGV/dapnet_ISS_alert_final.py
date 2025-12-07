b5c8c3ad6069ea935c548687857b106fd8625132
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-12-07 11:53:55 +0100

    Update run_ISS_alert.bat with folder comment
    Added a comment to specify the folder containing the .py file.

--- run_ISS_alert.bat
+++ run_ISS_alert.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
-cd /d "C:\Scripts\DAPNET"
+cd /d "C:\Scripts\DAPNET" #choose your folder who contain the .py file
+
 py "dapnet_ISS_alert.py" >> "C:\Scripts\DAPNET\dapnet_ISS_alert.log" 2>&1
